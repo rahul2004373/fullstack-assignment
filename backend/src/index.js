3e8fcf17f3a924644201a35e6b97d01da965efed
rahul2004373 <kanojiarahul2004@gmailcom>
2026-09-04 19:37:52 +0530

    updated cors issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://localhost:3000", "https://fullstack-assignment-sigma-eight.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
